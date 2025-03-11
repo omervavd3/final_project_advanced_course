555ff3ce10fb236a6e8c2707248d29327df09855
omervavd3 <omervavd3@gmail.com>
2025-03-11 21:36:48 +0200

    auth test

--- back/src/tests/auth.test.ts
+++ back/src/tests/auth.test.ts
@@ -2,10 +2,8 @@ import request from "supertest";
 import initApp from "../server";
 import mongoose from "mongoose";
 import UserModel from "../models/userModel";
-import e, { Express } from "express";
-import * as testJSON from "./testPost.json";
-import { access } from "fs";
-import exp from "constants";
+import { Express } from "express";
+import { profile } from "console";
 
 var app: Express;
 
@@ -13,11 +11,15 @@ beforeAll(async () => {
   console.log("beforeAll");
   app = await initApp();
   await UserModel.deleteOne({ email: testUser.email });
+  await UserModel.deleteOne({ email: testUser2.email });
+  await UserModel.deleteOne({ email: "test" });
 });
 
 afterAll(async () => {
   console.log("afterAll");
   await UserModel.deleteOne({ email: testUser.email });
+  await UserModel.deleteOne({ email: testUser2.email });
+  await UserModel.deleteOne({ email: "test" });
   mongoose.connection.close();
 });
 
@@ -25,6 +27,7 @@ type User = {
   email: string;
   password: string;
   userName: string;
+  profileImageUrl: string;
   _id?: string;
   refreshToken?: string;
   accessToken?: string;
@@ -34,12 +37,21 @@ const testUser: User = {
   email: "test@example.com",
   password: "1234",
   userName: "name test",
+  profileImageUrl: "https://www.google.com",
+};
+
+const testUser2: User = {
+  email: "test@example2.com",
+  password: "1234",
+  userName: "name test 3",
+  profileImageUrl: "https://www.google.com",
 };
 
 const userForRefresh: User = {
   email: "testRefresh@example.com",
   password: "4321",
   userName: "name test 2",
+  profileImageUrl: "https://www.google.com",
   refreshToken: "123124534586756453",
   accessToken: "123124534586756453",
 };
@@ -50,6 +62,7 @@ describe("User Tests", () => {
       email: testUser.email,
       password: testUser.password,
       userName: testUser.userName,
+      profileImageUrl: testUser.profileImageUrl,
     });
     expect(response.statusCode).toBe(201);
     expect(response.body.password).not.toBe(testUser.password);
@@ -60,6 +73,7 @@ describe("User Tests", () => {
       email: testUser.email,
       password: testUser.password,
       userName: testUser.userName,
+      profileImageUrl: testUser.profileImageUrl,
     });
     expect(response.statusCode).toBe(400);
   });
@@ -86,39 +100,170 @@ describe("User Tests", () => {
   });
 
   test("Auth test change password with wrong password", async () => {
-    const response = await request(app).put("/auth/updatePassword").set({
-      Authorization: `JWT ${testUser.accessToken}`,
-    }).send({
-      email: testUser.email,
-      oldPassword: "45678",
-      newPassword: "4321",
-    })
+    const response = await request(app)
+      .put("/auth/updatePassword")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        email: testUser.email,
+        oldPassword: "45678",
+        newPassword: "4321",
+      });
     expect(response.statusCode).not.toBe(200);
-  })
+  });
 
-  test("Auth test change password without all params", async () => {
-    const response = await request(app).put("/auth/updatePassword").set({
-      Authorization: `JWT ${testUser.accessToken}`,
-    }).send({
-      email: null,
-      oldPassword: null,
-      newPassword: "4321",
-    })
-    expect(response.statusCode).not.toBe(200);
-  })
-
-  test("Auth test change password", async () => {
-    const response = await request(app).put("/auth/updatePassword").set({
-      Authorization: `JWT ${testUser.accessToken}`,
-    }).send({
+  test("Auth test update without authorization", async () => {
+    const response = await request(app).put("/auth").send({
       email: testUser.email,
       oldPassword: testUser.password,
       newPassword: "4321",
-    })
+    });
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("Auth test update with wrong authorization", async () => {
+    const response = await request(app)
+      .put("/auth")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}1`,
+      })
+      .send({
+        email: testUser.email,
+        oldPassword: testUser.password,
+        newPassword: "4321",
+      });
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("Auth test update with an existing user name and email", async () => {
+    const response = await request(app).post("/auth/register").send({
+      email: testUser2.email,
+      password: testUser2.password,
+      userName: testUser2.userName,
+      profileImageUrl: testUser2.profileImageUrl,
+    });
+    expect(response.statusCode).toBe(201);
+
+    const response2 = await request(app)
+      .put("/auth")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        password: testUser.password,
+        email: testUser.email,
+        userName: testUser2.userName,
+      });
+    expect(response2.statusCode).toBe(400);
+
+    const response3 = await request(app)
+      .put("/auth")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        password: testUser.password,
+        email: testUser2.email,
+        userName: testUser.userName,
+      });
+    expect(response3.statusCode).toBe(400);
+  });
+
+  test("Auth test update with wrong password and without password", async () => {
+    const response = await request(app)
+      .put("/auth")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        email: testUser.email,
+      });
+    expect(response.statusCode).not.toBe(200);
+
+    const response2 = await request(app)
+      .put("/auth")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        password: "wrongPassword",
+        email: testUser.email,
+      });
+    expect(response2.statusCode).not.toBe(200);
+  });
+
+  test("Auth test update", async () => {
+    const response = await request(app)
+      .put("/auth")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        email: "newEmail@email.com",
+        password: testUser.password,
+        userName: "newName",
+        profileImageUrl: "https://www.google2.com",
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.userName).toBe("newName");
+    testUser.userName = "newName";
+    testUser.email = "newEmail@email.com";
+    testUser.profileImageUrl = "https://www.google2.com";
+  });
+
+  test("Auth test get profile image and name", async () => {
+    const response = await request(app)
+      .get("/auth/getProfileImageUrlAndName")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.userName).toBe(testUser.userName);
+    expect(response.body.profileImageUrl).toBe(testUser.profileImageUrl);
+  });
+
+  test("Auth test get user info", async () => {
+    const response = await request(app)
+      .get("/auth/getUserInfo")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.userName).toBe(testUser.userName);
+    expect(response.body.email).toBe(testUser.email);
+    expect(response.body.profileImageUrl).toBe(testUser.profileImageUrl);
+  });
+
+  test("Auth test change password without all params", async () => {
+    const response = await request(app)
+      .put("/auth/updatePassword")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        email: null,
+        oldPassword: null,
+        newPassword: "4321",
+      });
+    expect(response.statusCode).not.toBe(200);
+  });
+
+  test("Auth test change password", async () => {
+    const response = await request(app)
+      .put("/auth/updatePassword")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        email: testUser.email,
+        oldPassword: testUser.password,
+        newPassword: "4321",
+      });
     expect(response.statusCode).toBe(200);
     expect(response.body.password).not.toBe(testUser.password);
     testUser.password = "4321";
-  })
+  });
 
   test("Auth test login with wrong password", async () => {
     const response = await request(app).post("/auth/login").send({
@@ -162,7 +307,7 @@ describe("User Tests", () => {
       await user.save();
     }
     expect(response.statusCode).not.toBe(200);
-  })
+  });
 
   test("Auth test refresh token", async () => {
     const response = await request(app)
@@ -248,6 +393,52 @@ describe("User Tests", () => {
     expect(response.statusCode).toBe(200);
   });
 
+  test("Auth test delete user with wrong password", async () => {
+    const response = await request(app)
+      .delete("/auth/deleteUser")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      })
+      .send({
+        password: "wrongPassword",
+      });
+    expect(response.statusCode).not.toBe(200);
+  });
+
+  test("Auth test delete user without password", async () => {
+    const response = await request(app)
+      .delete("/auth/deleteUser")
+      .set({
+        Authorization: `JWT ${testUser.accessToken}`,
+      });
+    expect(response.statusCode).not.toBe(200);
+  });
+
+  test("Auth test delete user", async () => {
+    const response = await request(app).post("/auth/register").send({
+      email: "test",
+      password: "test",
+      userName: "test",
+      profileImageUrl: "test",
+    });
+    expect(response.statusCode).toBe(201);
+    const response2 = await request(app).post("/auth/login").send({
+      email: "test",
+      password: "test",
+    });
+    expect(response2.statusCode).toBe(200);
+    const accessToken = response2.body.accessToken;
+    const response3 = await request(app)
+      .delete("/auth/deleteUser")
+      .set({
+        Authorization: `JWT ${accessToken}`,
+      })
+      .send({
+        password: "test",
+      });
+    expect(response3.statusCode).toBe(200);
+  });
+
   test("Auth test logout when user does not exist", async () => {
     await UserModel.deleteOne({ email: testUser.email });
     const response = await request(app)
@@ -256,7 +447,7 @@ describe("User Tests", () => {
         Authorization: `JWT ${testUser.refreshToken}`,
       });
     expect(response.statusCode).not.toBe(200);
-  })
+  });
 
   test("Auth refresh when user does not exist", async () => {
     const response = await request(app)
@@ -265,5 +456,5 @@ describe("User Tests", () => {
         Authorization: `JWT ${testUser.refreshToken}`,
       });
     expect(response.statusCode).not.toBe(200);
-  })
+  });
 });
