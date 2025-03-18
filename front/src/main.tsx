0f4104120eb5f0066be2316ea61c7954cb9839b4
omervavd3 <omervavd3@gmail.com>
2025-03-18 17:14:33 +0200

    dev

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; 
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import HomePage from "./components/HomePage";
@@ -8,17 +8,16 @@ import EditUser from "./components/EditUser";
 import EditPost from "./components/EditPost";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<HomePage/>} />
-        <Route path="/editUser" element={<EditUser />} />
-        <Route path="editPost/:id" element={<EditPost />} />
-
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/editUser" element={<EditUser />} />
+          <Route path="editPost/:id" element={<EditPost />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
 );
